8b5f531468e811347b7f6e95c68db1a4c3eb9e86
junyu0577 <junyu0577@163.com>
2018-06-21 16:58:15 +0800

    update solution

--- solution/src/main/java/com/github/junyu/solution/easy/string/_008_Implement_strStr.java
+++ solution/src/main/java/com/github/junyu/solution/easy/string/_008_Implement_strStr.java
@@ -28,18 +28,57 @@ public class _008_Implement_strStr {
     This is consistent to C's strstr() and Java's indexOf().
     */
 
+//    /**
+//     * 暴力求解.. 按顺序挨个对比字符串,效率低下
+//     * @param haystack
+//     * @param needle
+//     * @return
+//     */
+//    public static int strStr(String haystack, String needle) {
+//        if (needle == null || "".equals(needle))
+//            return 0;
+//        if (needle.length() > haystack.length())
+//            return -1;
+//        int count;
+//        for (int i = 0; i < haystack.length(); i++) {
+//            count = 0;
+//            for (int j = 0; j < needle.length() && j + i < haystack.length(); j++) {
+//                if (haystack.charAt(i + j) == needle.charAt(j)) {
+//                    count++;
+//                    if (count == needle.length())
+//                        return i;
+//                } else {
+//                    break;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
 
+    /**
+     * KMP算法
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
     public static int strStr(String haystack, String needle) {
-        //TODO
-        if (needle == null || needle.length() == 0) {
+        if (needle == null || "".equals(needle))
             return 0;
-        }
-        return haystack.indexOf(needle);
+        if (needle.length() > haystack.length())
+            return -1;
+        //TODO
+        return -1;
     }
 
     public static void main(String[] args) {
 //        String haystack = "hello", needle = "ll";
-        String haystack = "aaaaa", needle = "bba";
+//        String haystack = "aaaaa", needle = "bba";
+//        String haystack = "aaaab", needle = "b";
+//        String haystack = "mississippi", needle = "issipi";
+//        String haystack = "bbaa", needle = "aab";
+        String haystack = "mississippi", needle = "issip";
+
         System.out.println(strStr(haystack, needle));
     }
 }
