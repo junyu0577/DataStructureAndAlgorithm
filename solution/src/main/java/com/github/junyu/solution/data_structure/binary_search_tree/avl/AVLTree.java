d7660d16c71bf85779281e2c4b47e4f91804f1d3
junyu0577 <junyu0577@163.com>
2018-07-28 22:19:56 +0800

    update AVLTree

--- solution/src/main/java/com/github/junyu/solution/data_structure/binary_search_tree/avl/AVLTree.java
+++ solution/src/main/java/com/github/junyu/solution/data_structure/binary_search_tree/avl/AVLTree.java
@@ -1,13 +1,16 @@
 package com.github.junyu.solution.data_structure.binary_search_tree.avl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 平衡的二叉搜索树
  * 节点大于左孩子，小于右孩子
  * 左右子树高度差不能超过1
+ *
+ * 时间复杂度O(logN)
  *
  * @author ShaoJunyu
  * @version $Id$
@@ -82,9 +85,9 @@ public class AVLTree {
     private boolean isBalanceTree(Node node) {
         if (node == null)
             return true;
-        if (Math.abs(getBalanceFactor(root)) > 1)
+        if (Math.abs(getBalanceFactor(node)) > 1)
             return false;
-        return isBalanceTree(root.leftChild) && isBalanceTree(root.rightChild);
+        return isBalanceTree(node.leftChild) && isBalanceTree(node.rightChild);
     }
 
     /**
@@ -132,10 +135,91 @@ public class AVLTree {
             node.leftChild = insert(key, value, node.leftChild);
         }
 
-        node.height = Math.max(getHeight(node.leftChild), getHeight(node.rightChild)) + 1;
-        if (Math.abs(getBalanceFactor(node)) > 1)
-            System.out.println(getBalanceFactor(node));
+        //修正字数深度
+        node.height = Math.max(getHeight(node.leftChild),getHeight(node.rightChild))+1;
+
+        //balance
+        int balanceFactor = getBalanceFactor(node);
+
+        //LL
+        if (balanceFactor>1 && getBalanceFactor(node.leftChild)>=0){
+            return rightRotate(node);
+        }
+
+        //RR
+        if (balanceFactor<-1 && getBalanceFactor(node.rightChild)<=0){
+            return leftRotate(node);
+        }
+
+        //LR
+        if (balanceFactor>1 && getBalanceFactor(node.leftChild)<0){
+            node.leftChild = leftRotate(node.leftChild);
+            return rightRotate(node);
+        }
+
+        //RL
+        if (balanceFactor<-1 && getBalanceFactor(node.rightChild)>0){
+            node.rightChild = rightRotate(node.rightChild);
+            return leftRotate(node);
+        }
+
         return node;
+    }
+
+    /**
+     * 右旋转
+     * 发生在元素被插入在左侧的左侧 破坏了平衡因子
+     *
+     *            y                        x
+     *          /   \                    /  \
+     *         x    T4                 z      y
+     *       /  \                     / \    / \
+     *      z  T3         ----->    T1  T2  T3  T4
+     *     /    \
+     *    T1    T2
+     *
+     * @param y
+     * @return
+     */
+    private Node rightRotate(Node y) {
+        Node x = y.leftChild;
+        Node T3 = x.rightChild;
+
+        x.rightChild = y;
+        y.leftChild = T3;
+
+        y.height = Math.max(getHeight(y.leftChild),getHeight(y.rightChild))+1;
+        y.height = Math.max(getHeight(x.leftChild),getHeight(x.rightChild))+1;
+
+        return x;
+    }
+
+    /**
+     * 左旋转
+     * 发生在元素被插入在右侧的右侧 破坏了平衡因子
+     *
+     *           y                        x
+     *         /   \                    /  \
+     *        T1     x                y      z
+     *              / \              / \    / \
+     *            T2   z   ----->   T1  T2  T3  T4
+     *           / \
+     *          T3  T4
+     *
+     * @param y
+     * @return
+     */
+    private Node leftRotate(Node y) {
+       Node x = y.rightChild;
+       Node T2 = x.leftChild;
+
+       x.leftChild = y;
+       y.rightChild = T2;
+
+       y.height = Math.max(getHeight(y.leftChild),getHeight(y.rightChild))+1;
+       x.height = Math.max(getHeight(x.leftChild),getHeight(x.rightChild))+1;
+
+       return x;
     }
 
     public boolean contain(int key) {
@@ -172,15 +256,52 @@ public class AVLTree {
         }
     }
 
+    private void levelOrder() {
+        levelOrder(root);
+    }
+
+    private void inOrder() {
+
+        inOrder(root);
+
+    }
+
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+        inOrder(node.leftChild);
+        System.out.print(node.value + "\t");
+        inOrder(node.rightChild);
+    }
+
+
+    private void levelOrder(Node node) {
+        if (node == null)
+            return;
+
+        Queue<Node> q = new LinkedList();
+        q.add(node);
+        while (!q.isEmpty()) {
+            Node n = q.remove();
+            System.out.print(n.value + "\t");
+            if (n.leftChild != null)
+                q.add(n.leftChild);
+
+            if (n.rightChild != null)
+                q.add(n.rightChild);
+        }
+
+    }
+
 
     public static void main(String[] args) {
         AVLTree bst = new AVLTree();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 100; i++) {
             bst.insert(i, String.valueOf(i));
         }
-        for (int i = 0; i < 6; i++) {
-            System.out.println(bst.contain(i));
-        }
+//        for (int i = 0; i < 6; i++) {
+//            System.out.println(bst.contain(i));
+//        }
 
 //        for (int i = 0; i < 6; i++) {
 //            System.out.println("value:" + bst.value(i));
@@ -188,6 +309,10 @@ public class AVLTree {
 
         System.out.println("is balance : " + bst.isBalanceTree());
         System.out.println("is BST : " + bst.isBST());
+
+        bst.levelOrder();
+        System.out.println();
+        bst.inOrder();
 
     }
 }
