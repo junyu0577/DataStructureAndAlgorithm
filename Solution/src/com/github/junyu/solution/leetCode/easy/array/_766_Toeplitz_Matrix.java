a804fe9d1727959a7cf3bf73515553287870c085
junyu0577 <junyu0577@163.com>
2018-08-17 16:26:19 +0800

    add solution 766 and 867

--- Solution/src/com/github/junyu/solution/leetCode/easy/array/_766_Toeplitz_Matrix.java
+++ Solution/src/com/github/junyu/solution/leetCode/easy/array/_766_Toeplitz_Matrix.java
@@ -1,58 +1,119 @@
 package com.github.junyu.solution.leetCode.easy.array;
 
-import java.util.Arrays;
-
 public class _766_Toeplitz_Matrix {
 
-/*
-    Given a matrix A, return the transpose of A.
+   /* A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
 
-    The transpose of a matrix is the matrix flipped over
-        it's main diagonal, switching the row and column indices of the matrix.
-
+    Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
 
 
     Example 1:
 
-    Input: [[1,2,3],[4,5,6],[7,8,9]]
-    Output: [[1,4,7],[2,5,8],[3,6,9]]
+    Input:
+    matrix = [
+            [1,2,3,4],
+            [5,1,2,3],
+            [9,5,1,2]
+            ]
+    Output: True
+    Explanation:
+    In the above grid, the diagonals are:
+            "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+    In each diagonal all elements are the same, so the answer is True.
     Example 2:
 
-    Input: [[1,2,3],[4,5,6]]
-    Output: [[1,4],[2,5],[3,6]]
+    Input:
+    matrix = [
+            [1,2],
+            [2,2]
+            ]
+    Output: False
+    Explanation:
+    The diagonal "[1, 2]" has different elements.
 
+            Note:
 
-    Note:
+    matrix will be a 2D array of integers.
+    matrix will have a number of rows and columns in range [1, 20].
+    matrix[i][j] will be integers in range [0, 99].
 
-            1 <= A.length <= 1000
-            1 <= A[0].length <= 1000
+    Follow up:
+
+    What if the matrix is stored on disk, and the memory is limited such that you can only load at
+    most one row of the matrix into the memory at once?
+    What if the matrix is so large that you can only load up a partial row into the memory at once?
     */
 
+//    /**
+//     * 查看矩阵中，左上到右下的对角线的所有元素是否相同，只要有一条对角线不相同就返回false
+//     * 先扫描列，如第一列 0 0, 1 1 , 2 2，都相同再看下一行，只要遍历完第一列就可以。
+//     * 扫完列再扫行，由于第一个元素已经扫后，可以直接跳过，同理，也只需要扫完第一行
+//     *
+//     * @param matrix
+//     * @return
+//     */
+//    public boolean isToeplitzMatrix(int[][] matrix) {
+//        for (int k = 0; k < matrix.length - 1; k++) {
+//            int j = 0 + k;
+//            int i = 0;
+//            while (j < matrix.length - 1 && i < matrix[0].length - 1) {
+//                if (matrix[j][i] != matrix[j + 1][i + 1])
+//                    return false;
+//                i++;
+//                j++;
+//            }
+//        }
+//
+//        for (int k = 1; k < matrix[0].length - 1; k++) {
+//            int j = 0;
+//            int i = 0 + k;
+//            while (i < matrix[0].length - 1 && j < matrix.length - 1) {
+//                if (matrix[j][i] != matrix[j + 1][i + 1])
+//                    return false;
+//                i++;
+//                j++;
+//            }
+//        }
+//
+//        return true;
+//    }
+
     /**
-     * 交换矩阵的行索引与列索引
+     * 解法二
+     * 从1 1，这个位置开始遍历，和左上角的元素做一次比较，如果不同就返回，相同就继续比较下一个元素
      *
-     * @param A
+     * @param matrix
      * @return
      */
-    public int[][] transpose(int[][] A) {
-        int[][] res = new int[A[0].length][A.length];
-        for (int i = 0; i < A[0].length; i++) {
-            for (int j = 0; j < A.length; j++) {
-                res[i][j] = A[j][i];
+    public boolean isToeplitzMatrix(int[][] matrix) {
+
+        if (matrix.length == 1 || matrix[0].length == 1) {//只有一列 或者一行的情况
+            return true;
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1])
+                    return false;
             }
         }
 
-        return res;
+        return true;
     }
 
     public static void main(String[] args) {
-//        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}};
-        _766_Toeplitz_Matrix toeplitz_matrix = new _766_Toeplitz_Matrix();
-        int[][] res = toeplitz_matrix.transpose(arr);
-        for (int i = 0; i < res.length; i++) {
-            System.out.println(Arrays.toString(res[i]));
-        }
-    }
+//        int arr[][] = {{1, 2, 3, 4},
+//                {5, 1, 2, 3},
+//                {9, 5, 1, 2}};
+        int arr[][] = {{1, 2},
+                {2, 2}};
 
+//        int arr [][] = {{18},{66}};
+//        int arr[][] = {{97, 97}, {80, 97}, {10, 80}};
+//        int arr[][] = {{97, 97, 97}, {80, 97, 97}};
+//        int arr[][] = {{65, 98, 57}};
+//        int arr[][] = {{36, 59, 71, 15, 26, 82, 87}, {56, 36, 59, 71, 15, 26, 82}, {15, 0, 36, 59, 71, 15, 26}};
+        _766_Toeplitz_Matrix toeplitz_matrix = new _766_Toeplitz_Matrix();
+        System.out.println(toeplitz_matrix.isToeplitzMatrix(arr));
+    }
 }
