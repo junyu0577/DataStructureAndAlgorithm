fa959b59a73f9f9ba157a6f08bea594b6db42973
junyu0577 <junyu0577@163.com>
2020-05-28 22:30:16 +0800

    update solution 1185

--- Solution/src/com/github/junyu/solution/leetCode/easy/math/_1185_Day_of_the_Week.java
+++ Solution/src/com/github/junyu/solution/leetCode/easy/math/_1185_Day_of_the_Week.java
@@ -30,27 +30,68 @@ public class _1185_Day_of_the_Week {
     The given dates are valid dates between the years 1971 and 2100.*/
 
     /**
-     * 给定的日期是星期几
-     * 根据基姆拉尔森的日期公式
+     * 如果已经知道1971-01-01为周五（作为起始时间），那么可以先计算给定的日期距离距离起始时间的天数，
+     * 将天数mod7以后，余数的值就是ans数组对应角标的星期。
+     *
      * @param day
      * @param month
      * @param year
      * @return
      */
     public String dayOfTheWeek(int day, int month, int year) {
-        String[] ans = new String[]{ "Monday",
-                "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday",};
-        if ((month == 1) || (month == 2)) {
-            month += 12;
-            year--;
+        int[] daysOfMonth = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] ans = new String[]{"Friday", "Saturday", "Sunday", "Monday",
+                "Tuesday", "Wednesday", "Thursday"};
+        int days = 0;
+        for (int i = 1971; i < year; i++) {
+            if (isLeapYear(i))
+                days += 366;
+            else
+                days += 365;
         }
-        return ans[(day + 2 * month + 3 * (month + 1) / 5 + year + year / 4 - year / 100 + year / 400) % 7];
+
+        for (int i = 1; i < month; i++) {
+            days += daysOfMonth[i - 1];
+            if (i == 2 && isLeapYear(year))
+                days++;
+        }
+
+        days += day - 1;
+
+        return ans[days % 7];
     }
+
+    private boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            return true;
+        return false;
+    }
+
+//    /**
+//     * 给定的日期是星期几
+//     * 根据基姆拉尔森的日期公式
+//     * @param day
+//     * @param month
+//     * @param year
+//     * @return
+//     */
+//    public String dayOfTheWeek(int day, int month, int year) {
+//        String[] ans = new String[]{ "Monday",
+//                "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday",};
+//        if ((month == 1) || (month == 2)) {
+//            month += 12;
+//            year--;
+//        }
+//        return ans[(day + 2 * month + 3 * (month + 1) / 5 + year + year / 4 - year / 100 + year / 400) % 7];
+//    }
+
 
     public static void main(String[] args) {
         _1185_Day_of_the_Week test = new _1185_Day_of_the_Week();
         System.out.println(test.dayOfTheWeek(31, 8, 2019));
         System.out.println(test.dayOfTheWeek(18, 7, 1999));
         System.out.println(test.dayOfTheWeek(15, 8, 1993));
+        System.out.println(test.dayOfTheWeek(1, 1, 1971));
+        System.out.println(test.dayOfTheWeek(29, 2, 2016));
     }
 }
